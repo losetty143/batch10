bd179e37edffa0f7c847b68cb4dc3c56a4130367
losetty143 <42094916+losetty143@users.noreply.github.com>
2021-06-02 18:48:33 +0100

    Update Application.java

--- src/main/java/com/Application.java
+++ src/main/java/com/Application.java
@@ -6,7 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application extends SpringBootServletInitializer {
+public class Application {
 	private static final Logger log = LoggerFactory.getLogger(Application.class);
 
 	public static void main(String[] args) {
